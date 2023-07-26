11b693a0c6c11cf4e45a697750c2baeb3980322a
YC2023 <cgl@88.com>
2023-07-26 08:43:17 +0800

    自动备份

--- 自动》拉取》再判断》上传.bat
+++ 自动》拉取》再判断》上传.bat
@@ -2,6 +2,11 @@
 :: 用于控制环境变量的扩展行为
 setlocal enabledelayedexpansion  
 
+
+:: 改变当前的工作目录     :: /d选项允许你在改变工作目录时跨驱动器进行切换。
+cd /d E:\Users\Dell\Desktop\笔记
+
+
 :: 拉取最新的变更
 git pull
 :: %ERRORLEVEL%是一个环境变量，它会存储最后一次执行的命令的退出状态或错误等级。通常情况下，如果命令成功执行，%ERRORLEVEL%将会被设置为0，否则它将会被设置为非零值。
