743c50e42576e94254621f7da1e6b3626c153cdf
ChenGuangLong <chenguanglong@timeexpress.com.cn>
2024-04-23 17:38:36 +0800

    优化生成博客py

---
+++ 重新生成博客文件夹.py
@@ -0,0 +1,25 @@
+import os
+import shutil  		# 删除非空文件夹
+import subprocess	# 执行其他py
+
+
+# 进去
+os.chdir('./已整理笔记')
+print(os.getcwd())
+
+# 先尝试删除“博客md”文件夹 
+try:
+	shutil.rmtree('./博客md')
+except FileNotFoundError:
+	print("博客md 不存在  不用删除了")
+		
+# 获取当前目录
+current_directory = os.path.dirname(os.path.abspath(__file__))
+
+# 执行 getBlogMd.py
+subprocess.run(["python", os.path.join(current_directory, "已整理笔记/getBlogMd.py")], check=True)
+
+# 执行 getMdList.py
+subprocess.run(["python", os.path.join(current_directory, "已整理笔记/getMdList.py")], check=True)
+
+print("=====完成重新生成博客=====")
