5b0854feab792098236fc8113aca02161545d8f5
仰晨 <cgl556@88.com>
2024-05-04 23:20:00 +0800

    py 生成排序问题解决

--- 已整理笔记/getMdList - old.py
+++ 已整理笔记/getMdList - old.py
@@ -1,33 +1,33 @@
 import os
 import json
 
-def sort_key(filename):
-    # 提取文件名开头的数字，用于排序
-    num = int(filename.split('.')[0].strip())
-    return num
-
 def scan_md_folder(path):
     """
     扫描指定路径下的第一层文件夹和对应的 .md 文件
+    
     :param path: 要扫描的路径
     :return: 包含文件夹和 .md 文件列表的嵌套列表
     """
     result = []
+
     # 获取第一层文件夹，排除以点开头的文件夹
     folders = [f for f in os.listdir(path) if os.path.isdir(os.path.join(path, f)) and not f.startswith('.')]
+
     for folder in folders:
         folder_path = os.path.join(path, folder)
+        
         # 获取文件夹下的 .md 文件列表，排除以点开头的文件
         md_files = [f for f in os.listdir(folder_path) if f.endswith('.md') and not f.startswith('.')]
-        # 对获取到的.md文件按数字进行排序
-        md_files.sort(key=sort_key)
+        
         # 将文件夹名和对应的 .md 文件列表作为一个子列表添加到结果列表中
         result.append([folder] + md_files)
+
     return result
 
 def save_to_json(data, output_file):
     """
     将数据保存为 JSON 文件
+    
     :param data: 要保存的数据
     :param output_file: 输出的 JSON 文件路径
     """
@@ -38,10 +38,14 @@ def save_to_json(data, output_file):
 if __name__ == '__main__':
     # 设置要扫描的 md 文件夹路径
     md_folder_path = './博客md'  # 替换为你的 md 文件夹路径
+    
     # 设置输出的 JSON 文件名
     output_file = './博客md/mdList.json'
+
     # 扫描 md 文件夹，获取文件夹和 .md 文件列表
     md_list = scan_md_folder(md_folder_path)
+    
     # 将结果保存到 JSON 文件中
     save_to_json(md_list, output_file)
+
     print(f'扫描完成，结果已保存到 {output_file} 文件中。')
