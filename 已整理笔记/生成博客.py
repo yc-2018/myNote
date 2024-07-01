c6e62b3609198512c3f6d95b64c2b1aa551971c2
仰晨 <cgl556@88.com>
2024-07-01 22:28:56 +0800

    不使用 subprocess 库 直接用os库来执行py

--- 已整理笔记/生成博客.py
+++ 已整理笔记/生成博客.py
@@ -1,6 +1,5 @@
 import os
 import shutil  		# 删除非空文件夹
-import subprocess	# 执行其他py
 
 
 # 先尝试删除“博客md”文件夹 
@@ -8,12 +7,9 @@ try:
 	shutil.rmtree('./博客md')
 except FileNotFoundError:
 	print("博客md 不存在  不用删除了")
-		
 
-# 执行 getBlogMd.py
-subprocess.run(["python", "getBlogMd.py"], check=True)
-
-# 执行 getMdList.py
-subprocess.run(["python","getMdList.py"], check=True)
+# 执行py
+os.system('python getBlogMd.py')
+os.system('python getMdList.py')
 
 print("=====完成重新生成博客=====")
